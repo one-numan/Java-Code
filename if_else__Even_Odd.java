6886360d4daf911c0153037f0081690a41a3dd34
Mohd Numan <48924562+one-numan1@users.noreply.github.com>
2023-04-09 06:16:55 +0530

    Even_Odd | If Else Ques.

---
+++ if_else__Even_Odd.java
@@ -0,0 +1,30 @@
+// Question Check Input Number is Even Odd
+// Take the Input from User
+//  Cost Price
+// Return How much profit or loss or no profit or no loss
+
+import java.util.Scanner;
+
+
+public class if_else__Even_Odd{
+    // Defing a Class
+public static void main(String args[]){
+    // Main Class
+    Scanner obj=new Scanner(System.in);
+    // Creating Object Scanner
+    int Number;
+    // Create Intger Variable
+    System.out.println("\n\nEnter The Number to Check Even or Odd");
+
+    Number=obj.nextInt();
+    // Taking User Input
+
+    // Logic : Check is divisble by 2 and Equal to 0 - If its True , then its Number is Even
+    if(Number%2==0)
+        System.out.println("Number Is Even");
+        // Printing Number is Even
+    else
+        // Printing Number is ODD
+        System.out.println("Number Is Odd");
+}
+}
